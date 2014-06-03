697fa30b97c2c3e7c60d2187bf34e1083fee42b9
David Rockwood <david.w.rockwood@gmail.com>
2014-06-03 15:39:01 -0700

    fixed links

--- public/js/duzuro.js
+++ public/js/duzuro.js
@@ -133,7 +133,7 @@ duzuroApp.filter('addAnchors', ['$sce',
 				return $sce.trustAsHtml(str.
 						replace(/</g, '&lt;').
 	                    replace(/>/g, '&gt;').
-	                    replace(/(https?:\/\/(www\.)?[-a-zA-Z0-9@:%._\+~#=]{2,256}\.[a-z]{2,4}\b[-a-zA-Z0-9@:%_\+.~#?&//=]*)/, '<a href="$1">$1</a>')
+	                    replace(/(https?:\/\/(www\.)?[-a-zA-Z0-9@:%._\+~#=]{2,256}\.[a-z]{2,4}\b[-a-zA-Z0-9@:%_\+.~#?&//=]*)/, '<a href="$1" target="_blank">$1</a>')
 				);
 			}
 		};
